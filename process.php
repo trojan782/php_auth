e2625ee5208d2e24b21603296a5652555a5a2667
david jacobs <davidjacobs522@gmail.com>
2021-04-12 23:19:49 +0100

    reverted array

--- process.php
+++ process.php
@@ -3,28 +3,20 @@ session_start();
 if (isset($_POST['signup'])) {
     $myfile = fopen("db.json", "a") or die("Unable to open file!");
     $txt =
-        // 'name' => array(
-        // array(
-        //     'name'           =>     $_POST['name'],
-        //     'email'          =>     $_POST["email"],
-        //     'password1'     =>     $_POST["password1"],
-        //     'password2'     =>     $_POST["password2"],
+        array(
+            'name'           =>     $_POST['name'],
+            'email'          =>     $_POST["email"],
+            'password1'     =>     $_POST["password1"],
+            'password2'     =>     $_POST["password2"],
 
-        // )
-        // );
-    $post_data = array(
-        'email' => array(
-            'name' => 1,
-            'password1' => 'AA',
-            'password2' => 'Hello',
-        )
-    );
-
+        );
+      
+     
     $final = json_encode($txt);
     fwrite($myfile, $final);
     fclose($myfile);
     // header("location:login.php");
-    
+
 }
 $data = file_get_contents("db.json");
 $json = json_decode($data, true);
@@ -37,9 +29,3 @@ foreach ($json as $key => $value) {
         }
     }
 }
-
-
-
-
-
-
