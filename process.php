04b3c121e90392c07b0d50ee6954f7bf97327d22
david jacobs <davidjacobs522@gmail.com>
2021-04-12 23:13:55 +0100

    new way for array

--- process.php
+++ process.php
@@ -1,28 +1,45 @@
 <?php
+session_start();
 if (isset($_POST['signup'])) {
     $myfile = fopen("db.json", "a") or die("Unable to open file!");
     $txt =
-        array(
-            'name'           =>     $_POST['name'],
-            'email'          =>     $_POST["email"],
-            'password1'     =>     $_POST["password1"],
-            'password2'     =>     $_POST["password2"],
+        // 'name' => array(
+        // array(
+        //     'name'           =>     $_POST['name'],
+        //     'email'          =>     $_POST["email"],
+        //     'password1'     =>     $_POST["password1"],
+        //     'password2'     =>     $_POST["password2"],
 
-        );
+        // )
+        // );
+    $post_data = array(
+        'email' => array(
+            'name' => 1,
+            'password1' => 'AA',
+            'password2' => 'Hello',
+        )
+    );
+
     $final = json_encode($txt);
     fwrite($myfile, $final);
     fclose($myfile);
+    // header("location:login.php");
+    
+}
+$data = file_get_contents("db.json");
+$json = json_decode($data, true);
+foreach ($json as $key => $value) {
+    if (!is_array($value)) {
+        echo $key . '=>' . $value . '<br/>';
+    } else {
+        foreach ($value as $key => $val) {
+            echo $key . '=>' . $val . '<br/>';
+        }
+    }
 }
 
 
-// $data = file_get_contents("db.json");
-// $json = json_decode($data, true);
-// foreach ($json as $key => $value) {
-//     if (!is_array($value)) {
-//         echo $key . '=>' . $value . '<br/>';
-//     } else {
-//         foreach ($value as $key => $val) {
-//             echo $key . '=>' . $val . '<br/>';
-//         }
-//     }
-// }
+
+
+
+
