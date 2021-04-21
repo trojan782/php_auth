baf20524107b4e5d8dee1c97a4912204abcb0750
david jacobs <davidjacobs522@gmail.com>
2021-04-21 22:45:53 +0100

    added id

--- new.php
+++ new.php
@@ -1,16 +1,17 @@
 <?php
-	
+session_start();
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 				
 	function get_data() {
 		// $name = $_POST['name'];
 		$file_name='db'. '.json';
-
+		//to check if the file exists
 		if(file_exists("$file_name")) {
 			$current_data=file_get_contents("$file_name");
 			$array_data=json_decode($current_data, true);
 							
 			$extra=array(
+				'id' => rand(1, 1000),
 				'name' => $_POST['name'],
 				'email' => $_POST['email'],
 				'password1' => password_hash($_POST['password1'], PASSWORD_DEFAULT),
@@ -35,9 +36,13 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 	$file_name='db'. '.json';
 	
 	if(file_put_contents("$file_name", get_data())) {
-		
+		echo '<h2 class="success1">You have successfully Registered</h2>';
+		// header("location: login.php");
 	}				
 	else {
 					
 	}
+
 }
+
+
